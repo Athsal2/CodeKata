69e99b92ce0693a7ce6a1902c2c9f96cd38dd37a
Athsal2 <49299200+Athsal2@users.noreply.github.com>
2019-07-08 19:27:47 +0530

    Create Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner sc=new Scanner(System.in);
+	    n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.print((i*n)+" ");
+			 
+}}}
