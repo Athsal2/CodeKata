c5e3d27ad129e22fce28e98426832ff489556883
Athsal2 <49299200+Athsal2@users.noreply.github.com>
2019-07-08 19:12:49 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n,fact=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			fact*=i;
+		}
+		System.out.println(fact);
+	
+	}
+
+}
